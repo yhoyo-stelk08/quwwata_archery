95905cbfcbee37a2cf868c03f63d49ae5adbe4ef
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2025-10-02 14:39:04 +0800

    feat: add carousel slide route to admin route

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\ProductImageController;
+use App\Http\Controllers\Admin\CarouselSlideController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.')->group(function () {
@@ -17,4 +18,8 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.'
     
     // Product Images CRUD
     Route::resource('product-images', ProductImageController::class);
+
+    // Carousel Slides
+    Route::resource('carousel-slides', CarouselSlideController::class);
+    Route::post('carousel-slides/reorder', [CarouselSlideController::class, 'reorder'])->name('carousel-slides.reorder');
 });
